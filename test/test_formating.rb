b7d3caacfd6fcfe0444f51ae1c3baf62e8966b2e
Randy Russell <ransolo@me.com>
2014-04-21 20:58:03 -0500

    "for real Do"

--- test/test_formating.rb
+++ test/test_formating.rb
@@ -1,43 +1,44 @@
-require 'formatting'
-require 'test/unit'
-
-class FormattingTest <MiniTest::Unit::TestCase
-  def test_print_header
-    header = print_header(4, 2014)
-    assert_equal "April 2014", header
-  end
-
-  # def test_conditionals_m_O_year_ok
-  #   con = conditionals(0, 2012)
-  #    assert_equal con, "ArgumentError: that's not a month dummy"
-  # end
-
-  # def test_month_arg_to_text
-  #   @m = month_arg_to_s
-  #   assert_equal "January", month_arg_to_s
-  # end
-  #
-  # def test_month_arg_to_text_double_figures
-  #   @m = month_arg_to_s
-  #   assert_equal "December", @m
-  # end
-  #
-  # def test_days
-  #   assert days = "Mo Tu We Th Fr Sa Su"
-  # end
-  #
-  # def test_Saturday_l_day_to_s
-  #   day = individual_day_to_s(01, 2012)
-  #   assert_equal "Sunday", day
-  # end
-  #
-
-  # def test_individual_day_to_s
-  #     day = individual_day_to_s(4, 2012)
-  #     assert_equal "Sunday", day
-  # end
-end
-# def test_find_weekday
-#   h = find_weekday(4, 2014)
-#   assert_equal 3, h
-# end
+# require 'formatting'
+# require 'test/unit'
+#
+# class FormattingTest <MiniTest::Unit::TestCase
+# #   def test_print_header
+# #     test = Month.new(04, 2014)
+# #     header = test.print_header
+# #     assert_equal header,
+# #   end
+# #
+# #   # def test_conditionals_m_O_year_ok
+# #   #   con = conditionals(0, 2012)
+# #   #    assert_equal con, "ArgumentError: that's not a month dummy"
+# #   # end
+# #
+# #   # def test_month_arg_to_text
+# #   #   @m = month_arg_to_s
+# #   #   assert_equal "January", month_arg_to_s
+# #   # end
+# #   #
+# #   # def test_month_arg_to_text_double_figures
+# #   #   @m = month_arg_to_s
+# #   #   assert_equal "December", @m
+# #   # end
+# #   #
+# #   # def test_days
+# #   #   assert days = "Mo Tu We Th Fr Sa Su"
+# #   # end
+# #   #
+# #   # def test_Saturday_l_day_to_s
+# #   #   day = individual_day_to_s(01, 2012)
+# #   #   assert_equal "Sunday", day
+# #   # end
+# #   #
+# #
+# #   # def test_individual_day_to_s
+# #   #     day = individual_day_to_s(4, 2012)
+# #   #     assert_equal "Sunday", day
+# #   # end
+# # end
+# # # def test_find_weekday
+# # #   h = find_weekday(4, 2014)
+# # #   assert_equal 3, h
+# # # end
---
+++ test/test_year.rb
@@ -0,0 +1,8 @@
+require 'formatting'
+require 'test/unit'
+require 'formatYear'
+
+class YearTest <MiniTest::Unit::TestCase
+
+
+end
